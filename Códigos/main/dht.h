c3f5a79ca3af8d58783ccd183f803f9fe7f178d5
Renata Ferreira <50049586+renataferreirasf@users.noreply.github.com>
2022-12-22 16:20:28 -0300

    Update dht.h

--- Códigos/main/dht.h
+++ Códigos/main/dht.h
@@ -1,24 +1,22 @@
-/*
-
-	DHT11 temperature sensor driver
-
-*/
-
 #ifndef DHT11_H_
 #define DHT11_H_
 
-#define SENSOR_TYPE DHT_TYPE_DHT11
+#include "driver/gpio.h"
 
-#define DHT_GPIO			21
+enum dht11_status {
+    DHT11_CRC_ERROR = -2,
+    DHT11_TIMEOUT_ERROR,
+    DHT11_OK
+};
 
-/**
- * Starts DHT11 sensor task
- */
+struct dht11_reading {
+    int status;
+    int temperature;
+    int humidity;
+};
 
-void DHT11_read(void);
-int _waitOrTimeout();
-int _sendStartSignal();
-int _checkResponse();
-void DHT11_init();
+void DHT11_init(gpio_num_t);
+
+struct dht11_reading DHT11_read();
 
 #endif
